bcf6c96f24f7e6581b07acd0a0e8d9d628edef5a
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-12-04 19:21:27 +0800

    chore: add Rollup plugins
    Includes resolve and copy plugins.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,22 @@
+import copy from 'rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+
 export default {
 	input: 'src/index.js',
+
 	output: {
-		file: 'dist/bundle.js',
+		file: 'dist/index.js',
 		format: 'esm'
-	}
+	},
+
+	plugins: [
+		resolve(),
+		copy({
+			targets: [
+				{ src: 'src/index.html', dest: 'dist' },
+				{ src: 'src/main.css', dest: 'dist' },
+				{ src: 'src/assets', dest: 'dist' }
+			]
+		})
+	]
 };
